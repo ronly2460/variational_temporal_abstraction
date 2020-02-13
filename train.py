07da2d13e1ca3d8df177e35c2c4fa35f37937270
lonlyness <takuma2460.0131@gmail.com>
2020-02-13 21:51:21 +0900

    change argperser to toml

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import sys
 import logging
 import numpy as np
+import toml
 import torch
 import torch.nn as nn
 from torch.optim import Adam
@@ -10,65 +11,46 @@ from utils import preprocess, postprocess, full_dataloader, log_train, log_test,
 LOGGER = logging.getLogger(__name__)
 
 
-def parse_args():
-    import argparse
-    parser = argparse.ArgumentParser(description="vta agr parser")
-    parser.add_argument('--seed', type=int, default=111)
-
-    # data size
-    parser.add_argument('--batch-size', type=int, default=64)
-    parser.add_argument('--seq-size', type=int, default=20)
-    parser.add_argument('--init-size', type=int, default=5)
-
-    # model size
-    parser.add_argument('--state-size', type=int, default=8)
-    parser.add_argument('--belief-size', type=int, default=128)
-    parser.add_argument('--num-layers', type=int, default=3)
-
-    # observation distribution
-    parser.add_argument('--obs-std', type=float, default=1.0)
-    parser.add_argument('--obs-bit', type=int, default=5)
-
-    # optimization
-    parser.add_argument('--learn-rate', type=float, default=0.0005)
-    parser.add_argument('--grad-clip', type=float, default=10.0)
-    parser.add_argument('--max-iters', type=int, default=500000)
-
-    # subsequence prior params
-    parser.add_argument('--seg-num', type=int, default=5)
-    parser.add_argument('--seg-len', type=int, default=8)
-
-    # gumbel params
-    parser.add_argument('--max-beta', type=float, default=1.0)
-    parser.add_argument('--min-beta', type=float, default=0.1)
-    parser.add_argument('--beta-anneal', type=float, default=100)
-
-    # log dir
-    parser.add_argument('--log-dir', type=str, default='./asset/log/')
-    return parser.parse_args()
-
-
 def set_exp_name(args):
     exp_name = 'hssm_maze'
-    exp_name += '_b{}'.format(args.batch_size)
-    exp_name += '_l{}_i{}'.format(args.seq_size, args.init_size)
-    exp_name += '_b{}_s{}_c{}'.format(args.belief_size, args.state_size, args.num_layers)
-    exp_name += '_gc{}_lr{}'.format(args.grad_clip, args.learn_rate)
-    exp_name += '_sg{}-{}'.format(args.seg_num, args.seg_len)
-    exp_name += '_std{}_bits{}'.format(args.obs_std, args.obs_bit)
-    exp_name += '_gum{}-{}-{}'.format(args.min_beta, args.max_beta, args.beta_anneal)
-    exp_name += '_seed{}'.format(args.seed)
+    exp_name += '_b{}'.format(args['batch_size'])
+    exp_name += '_l{}_i{}'.format(args['seq_size'], args['init_size'])
+    exp_name += '_b{}_s{}_c{}'.format(args['belief_size'], args['state_size'], args['num_layers'])
+    exp_name += '_gc{}_lr{}'.format(args['grad_clip'], args['learn_rate'])
+    exp_name += '_sg{}-{}'.format(args['seg_num'], args['seg_len'])
+    exp_name += '_std{}_bits{}'.format(args['obs_std'], args['obs_bit'])
+    exp_name += '_gum{}-{}-{}'.format(args['min_beta'], args['max_beta'], args['beta_anneal'])
+    exp_name += '_seed{}'.format(args['seed'])
     return exp_name
 
 
 def main():
-    # parse arguments
-    args = parse_args()
-
+    # configs
+    args = toml.load(open('config.toml'))['model']
+    
+    seed = args['seed']
+    batch_size = args['batch_size']
+    seq_size = args['seq_size']
+    init_size = args['init_size']
+    state_size = args['state_size']
+    belief_size = args['belief_size']
+    num_layers = args['num_layers']
+    obs_std = args['obs_std']
+    obs_bit = args['obs_bit']
+    learn_rate = args['learn_rate']
+    grad_clip = args['grad_clip']
+    max_iters = args['max_iters']
+    seg_num = args['seg_num']
+    seg_len = args['seg_len']
+    max_beta = args['max_beta']
+    min_beta = args['min_beta']
+    beta_anneal = args['beta_anneal']
+    log_dir = args['log_dir']
+    
     # fix seed
-    np.random.seed(args.seed)
-    torch.manual_seed(args.seed)
-    torch.cuda.manual_seed_all(args.seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
 
     # set logger
@@ -76,65 +58,65 @@ def main():
     logging.basicConfig(level=logging.INFO, format=log_format, stream=sys.stderr)
 
     # set size
-    seq_size = args.seq_size
-    init_size = args.init_size
+    seq_size = seq_size
+    init_size = init_size
 
     # set device as gpu
     device = torch.device('cuda', 0)
 
     # set writer
     exp_name = set_exp_name(args)
-    writer = SummaryWriter(args.log_dir + exp_name)
+    writer = SummaryWriter(log_dir + exp_name)
     LOGGER.info('EXP NAME: ' + exp_name)
 
     # load dataset
-    train_loader, test_loader = full_dataloader(seq_size, init_size, args.batch_size)
+    train_loader, test_loader = full_dataloader(seq_size, init_size, batch_size)
     LOGGER.info('Dataset loaded')
 
     # init models
-    model = EnvModel(belief_size=args.belief_size,
-                     state_size=args.state_size,
-                     num_layers=args.num_layers,
-                     max_seg_len=args.seg_len,
-                     max_seg_num=args.seg_num).to(device)
+    model = EnvModel(belief_size=belief_size,
+                     state_size=state_size,
+                     num_layers=num_layers,
+                     max_seg_len=seg_len,
+                     max_seg_num=seg_num).to(device)
     LOGGER.info('Model initialized')
 
     # init optimizer
     optimizer = Adam(params=model.parameters(),
-                     lr=args.learn_rate, amsgrad=True)
+                     lr=learn_rate, amsgrad=True)
 
     # test data
     pre_test_full_data_list = iter(test_loader).next()
-    pre_test_full_data_list = preprocess(pre_test_full_data_list.to(device), args.obs_bit)
+    pre_test_full_data_list = preprocess(pre_test_full_data_list.to(device), obs_bit)
 
     # for each iter
     b_idx = 0
-    while b_idx <= args.max_iters:
+    while b_idx <= max_iters:
         # for each batch
         for train_obs_list in train_loader:
             b_idx += 1
             # mask temp annealing
-            if args.beta_anneal:
-                model.state_model.mask_beta = (args.max_beta - args.min_beta) * 0.999 ** (b_idx / args.beta_anneal) + args.min_beta
+            if beta_anneal:
+                model.state_model.mask_beta = (max_beta - min_beta) * 0.999 ** (b_idx / beta_anneal) + min_beta
             else:
-                model.state_model.mask_beta = args.max_beta
+                model.state_model.mask_beta = max_beta
 
             ##############
             # train time #
             ##############
             # get input data
-            train_obs_list = preprocess(train_obs_list.to(device), args.obs_bit)
+            train_obs_list = preprocess(train_obs_list.to(device), obs_bit)
 
             # run model with train mode
             model.train()
             optimizer.zero_grad()
-            results = model(train_obs_list, seq_size, init_size, args.obs_std)
+            results = model(train_obs_list, seq_size, init_size, obs_std)
 
             # get train loss and backward update
             train_total_loss = results['train_loss']
             train_total_loss.backward()
-            if args.grad_clip > 0.0:
-                nn.utils.clip_grad_norm_(model.parameters(), args.grad_clip)
+            if grad_clip > 0.0:
+                nn.utils.clip_grad_norm_(model.parameters(), grad_clip)
             optimizer.step()
 
             # log
@@ -148,17 +130,17 @@ def main():
             if b_idx % 100 == 0:
                 # set data
                 pre_test_init_data_list = pre_test_full_data_list[:, :init_size]
-                post_test_init_data_list = postprocess(pre_test_init_data_list, args.obs_bit)
+                post_test_init_data_list = postprocess(pre_test_init_data_list, obs_bit)
                 pre_test_input_data_list = pre_test_full_data_list[:, init_size:(init_size + seq_size)]
-                post_test_input_data_list = postprocess(pre_test_input_data_list, args.obs_bit)
+                post_test_input_data_list = postprocess(pre_test_input_data_list, obs_bit)
 
                 with torch.no_grad():
                     ##################
                     # test data elbo #
                     ##################
                     model.eval()
-                    results = model(pre_test_full_data_list, seq_size, init_size, args.obs_std)
-                    post_test_rec_data_list = postprocess(results['rec_data'], args.obs_bit)
+                    results = model(pre_test_full_data_list, seq_size, init_size, obs_std)
+                    post_test_rec_data_list = postprocess(results['rec_data'], obs_bit)
                     output_img, output_mask = plot_rec(post_test_init_data_list,
                                                        post_test_input_data_list,
                                                        post_test_rec_data_list,
@@ -176,7 +158,7 @@ def main():
                     # full generation #
                     ###################
                     pre_test_gen_data_list, test_mask_data_list = model.full_generation(pre_test_init_data_list, seq_size)
-                    post_test_gen_data_list = postprocess(pre_test_gen_data_list, args.obs_bit)
+                    post_test_gen_data_list = postprocess(pre_test_gen_data_list, obs_bit)
 
                     # log
                     output_img = plot_gen(post_test_init_data_list, post_test_gen_data_list, test_mask_data_list)
