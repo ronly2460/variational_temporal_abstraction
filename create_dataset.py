5560b239ee334dc6da1dc0bf081220ec67ad9e9f
lonlyness <takuma2460.0131@gmail.com>
2020-02-16 16:57:21 +0900

    make it possible to choose fix or not

--- create_dataset.py
+++ create_dataset.py
@@ -4,30 +4,41 @@ import toml
 from keras.datasets import mnist
 
 
-def get_imgs(target_label):
+def get_imgs(target_label, fix=True, max_len=500):
     """
     mnistをロードして、target_labelに対応する画像を取り出す
     """
-    (x_train, y_train), (_, _) = mnist.load_data()
+    (x_train, y_train), _ = mnist.load_data()
     
-    files = []
+    imgs_sets = []
     for label in target_label:
-        files.append(x_train[np.where(y_train == label)][0])
+        if fix:
+            print('fix')
+            imgs_sets.append(x_train[np.where(y_train == label)][0])
+        else:
+            x_train, y_train = x_train[:max_len], y_train[:max_len]
+            imgs_sets.append(x_train[np.where(y_train == label)])
 
-    return files
+    return imgs_sets
 
 
-def preprocess(files):
+def preprocess(imgs_sets, fix=True):
     """
     paddingを行う (28x28 -> 32x32)
     """
-    imgs = []
-
-    for img in files:
-        img = np.pad(img, [2, 2])
-        imgs.append(img.flatten())
+    result = []
+    for imgs in imgs_sets:
+        if fix:
+            img = np.pad(imgs, [2, 2])
+            result.append(img.flatten())
+        else:
+            tmp = []
+            for img in imgs:
+                img = np.pad(img, [2,2])
+                tmp.append(img.flatten())
+            result.append(np.array(tmp))
                     
-    return imgs
+    return result
 
 
 def main():
@@ -39,35 +50,42 @@ def main():
     max_len = args['max_len']
     save_name = args['save_name']
     target_label = args['target_label']
-
+    fix = args['fix']
+    target_num = len(target_label)
+    
     # start
-    files = get_imgs(target_label)
-    imgs = preprocess(files)
+    imgs_sets = get_imgs(target_label, fix)
+    imgs_sets = preprocess(imgs_sets, fix)
 
     # データセット作成
-    file_num = len(files)
     cnt = 0
     data = []
     while cnt < data_length:
-
-        #どの数字の画像を使うか
-        idx = np.random.choice(file_num)
-
+        
         # 同じ画像が連続する枚数
-        num = np.random.choice(np.arange(min_len, max_len+1))
-
+        repeat_times = np.random.choice(np.arange(min_len, max_len+1))
         # data_lengthを超える場合
-        if cnt + num > data_length:
-            num = data_length - cnt
-
-        res = np.tile(imgs[idx], num)
+        if cnt + repeat_times > data_length:
+            repeat_times = data_length - cnt
+        
+        # どの数字の画像を使うか
+        num = np.random.choice(target_num)
+        
+        if fix:
+            res = np.tile(imgs_sets[num], repeat_times)
+        else:
+            # ある数字のどの画像を使うか
+            idx = np.random.choice(imgs_sets[num].shape[0])
+            res = np.tile(imgs_sets[num][idx], repeat_times)
+            
         data.append(res)
-        cnt = cnt + num
-
+        cnt = cnt + repeat_times
+    
     #reshape (-1, 32, 32) -> (-1, 1, 32, 32)
     data = np.concatenate(data).reshape(-1, 1, 32, 32)
     
     # 保存
+    print('fin')
     np.save(save_name, data)
 
 
